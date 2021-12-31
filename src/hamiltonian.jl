cf2d4e1a273695921e552b2a3e54e565d83ebe35
Satya Bade <bade.narayana@gmail.com>
2021-12-31 13:54:31 +0100

    fix CuHamiltonian

--- src/hamiltonian.jl
+++ src/hamiltonian.jl
@@ -14,6 +14,23 @@ mutable struct Hamiltonian{T<:Real}
         h.basis_r = const_op.basis_r
         h.const_op = const_op.data
         h.operators = [op.data for op in ops]
+        applicable(drives, rand(real(eltype(const_op.data)), length(operators + 1))...) ?
+        nothing : throw(ArgumentError)
+        h.drives = drives
+        h
+    end
+    function Hamiltonian(
+        const_op::mat,
+        basis_l::Basis,
+        basis_r::Basis,
+        operators::Vector{mat},
+        drives::Function,
+    ) where {T<:Real,mat<:AbstractMatrix{Complex{T}}}
+        h = new{T}()
+        h.basis_l = basis_l
+        h.basis_r = basis_r
+        h.const_op = const_op
+        h.operators = operators
         h.drives = drives
         h
     end
